09983ab7c37f935b37754b78fd9451eed7d3be66
Matheus Romano Bonfim <157503343+Mathues01@users.noreply.github.com>
2024-03-26 08:14:53 -0300

    atualizaçao

--- script+.js
+++ script+.js
@@ -10,7 +10,7 @@ function tabuada() {
         
         while (c <= 10) {
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
+            item.text = `${n} + ${c} = ${n+c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
