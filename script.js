b4e0fc9ec7310dbf9a17554eb93aa5b1dc9276ad
Matheus Romano Bonfim <157503343+Mathues01@users.noreply.github.com>
2024-04-02 18:24:14 -0300

    finazlida

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function tabuada() {
+function tabuadaMultiplicacao() {
     let num = document.getElementById('txn')
     let tab = document.getElementById('seltab')
     if (num.value.length == 0) {
@@ -7,6 +7,7 @@ function tabuada() {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ""
+        window.alert("Sua tabauda está pronta")
         
         while (c <= 10) {
             let item = document.createElement('option')
@@ -17,5 +18,85 @@ function tabuada() {
         }
     }
 }
+
+function tabuadaDivisao() {
+    let num = document.getElementById('txn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert("Digite um número!!")
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ""
+        window.alert("Sua tabauda está pronta")
+        
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} / ${c} = ${n/c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+function tabuadaSoma() {
+    let num = document.getElementById('txn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert("Digite um número!!")
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ""
+        window.alert("Sua tabauda está pronta")
+        
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} + ${c} = ${n+c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+function tabuadaSubtracao() {
+    let num = document.getElementById('txn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert("Digite um número!!")
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ""
+        window.alert("Sua tabauda está pronta")
+        
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} - ${c} = ${n-c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+
+
+ 
+
+
+
+ 
+
+
+ 
+
+
+ 
+
+
+
+ 
+
+
  
 
